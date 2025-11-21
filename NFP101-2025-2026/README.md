c4626e9636eab8ff79e0e3489409ebf8994c95d7
Moahmed Amine Sobhi <mohamed.amine.sobhi@etud.u-picardie.fr>
2025-11-21 14:51:03 +0100

    Recréation dépôt et ajout de tous les fichiers

---
+++ NFP101-2025-2026/README.md
@@ -0,0 +1,2 @@
+# NFP101-2025-2026
+Mon premier repo GIT-CNAM
---
+++ NFP101-2025-2026/TD02/exo1.py
@@ -0,0 +1,8 @@
+def majorite(nom, age):
+      if age >= 18:
+          print(f'Bonjour, {nom} est majeur')
+      else:
+          print(f'Bonjour, {nom} est mineur')
+
+
+majorite('Amine', 22)

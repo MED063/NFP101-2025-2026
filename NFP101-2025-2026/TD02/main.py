371cd0b3ba00d571dd1ce9bad43780e07c823f88
Moahmed Amine Sobhi <mohamed.amine.sobhi@etud.u-picardie.fr>
2025-11-21 15:46:13 +0100

    commit a annuler

---
+++ NFP101-2025-2026/TD02/main.py
@@ -0,0 +1,9 @@
+from exo2 import volumeSphere
+
+def main():
+      rayon = 5
+      resultat = volumeSphere(rayon)
+      print(f'Volume de la sphere (r={rayon}) : {resultat:.2f}')
+
+if __name__ == '__main__':
+      main()
